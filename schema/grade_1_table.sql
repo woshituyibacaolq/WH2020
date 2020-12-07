dbf151227f45579fe3164970a626445d543aef8d
lq <1749207602@qq.com>
2020-12-07 19:25:01 +0800

    first commit

---
+++ schema/grade_1_table.sql
@@ -0,0 +1,50 @@
+﻿DROP TABLE IF EXISTS student;
+CREATE TABLE IF NOT EXISTS student  (
+    sn       INTEGER,     --序号
+    no       VARCHAR(10), --学号
+    name     TEXT,        --姓名
+    gender   CHAR(1),     --性别(F/M/O)
+    enrolled DATE,        --入学时间
+    PRIMARY KEY(sn)
+);
+
+-- 给sn创建一个自增序号
+CREATE SEQUENCE seq_student_sn 
+    START 10000 INCREMENT 1 OWNED BY student.sn;
+ALTER TABLE student ALTER sn 
+    SET DEFAULT nextval('seq_student_sn');
+-- 学号唯一
+CREATE UNIQUE INDEX idx_student_no ON student(no);
+
+-- === 课程表
+DROP TABLE IF EXISTS course;
+CREATE TABLE IF NOT EXISTS course  (
+    sn       INTEGER,     --序号
+    no       VARCHAR(10), --课程号
+    name     TEXT,        --课程名称
+    place    VARCHAR(10),  --课程地点
+    week     TEXT,         --第几周 
+    class    TEXT,         --第几节
+    PRIMARY KEY(sn)
+);
+CREATE SEQUENCE seq_course_sn 
+    START 10000 INCREMENT 1 OWNED BY course.sn;
+ALTER TABLE course ALTER sn 
+    SET DEFAULT nextval('seq_course_sn');
+CREATE UNIQUE INDEX idx_course_no ON course(no);
+
+
+
+DROP TABLE IF EXISTS course_grade;
+CREATE TABLE IF NOT EXISTS course_grade  (
+    stu_sn INTEGER,     -- 学生序号
+    cou_sn INTEGER,     -- 课程序号
+    grade  NUMERIC(5,2), -- 最终成绩
+    PRIMARY KEY(stu_sn, cou_sn)
+);
+
+ALTER TABLE course_grade 
+    ADD CONSTRAINT stu_sn_fk FOREIGN KEY (stu_sn) REFERENCES student(sn);
+ALTER TABLE course_grade 
+    ADD CONSTRAINT cou_sn_fk FOREIGN KEY (cou_sn) REFERENCES course(sn);
+
---
+++ schema/grade_2_data.sql
@@ -0,0 +1,25 @@
+﻿DELETE FROM course_grade;
+DELETE FROM course;
+DELETE FROM student;
+
+INSERT INTO student (sn, no, name)  VALUES
+    (101, 'S001',  '张三'),
+    (102, 'S002',  '李四'), 
+    (103, 'S003',  '王五'),
+    (104, 'S004',  '马六');
+
+INSERT INTO course (sn, no, name, place, week, class)  VALUES 
+    (101, 'C01',  '高数', 'A111', '周一', '第一大节'), 
+    (102, 'C02',  '外语', 'B205', '周四', '第三大节'),
+    (103, 'C03',  '线代', 'C304', '周二', '第二大节'),
+    (104, 'C04',  '毛概', 'D108', '周五', '第四大节');
+
+
+INSERT INTO course_grade (stu_sn, cou_sn, grade)  VALUES 
+    (101, 101,  91), 
+    (102, 101,  89),
+    (103, 101,  90),
+    (101, 102,  89);
+
+
+    
